dea14773cd3bafab9b084038ae85ed5282cab347
Taiwo Adeyemi <tadeyemi004@outlook.com>
2023-09-21 06:48:56 -0700

    monty

---
+++ addque.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * aqueue - add /queue
+ * @n: newvalue/
+ * @head: stack head
+ * Return: void
+*/
+void aqueue(stack_t **head, int n)
+{
+	stack_t *newnode, *tmp;
+
+	tmp = *head;
+	newnode = malloc(sizeof(stack_t));
+	if (newnode == NULL)
+	{
+		printf("Error\n");
+	}
+	newnode->n = n;
+	newnode->next = NULL;
+	if (tmp)
+	{
+		while (tmp->next)
+        {
+			tmp = tmp->next;
+        }
+	}
+	if (!tmp)
+	{
+		*head = newnode;
+		newnode->prev = NULL;
+	}
+	else
+	{
+		tmp->next = newnode;
+		newnode->prev = tmp;
+	}
+}

24c3a016dd7bce977afdaf13a688c43dac0a93de
Taiwo Adeyemi <tadeyemi004@outlook.com>
2023-09-21 06:03:12 -0700

    monty

---
+++ add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _addn - addnodes 
+ * @head: head
+ * @n: new value
+ * Return: void
+*/
+void _addn(stack_t **head, int n)
+{
+
+	stack_t *newnode, *tmp;
+
+	tmp = *head;
+	newnode = malloc(sizeof(stack_t));
+	if (newnode == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (tmp)
+    {
+		tmo->prev = newnode;
+    }
+	newnode->n = n;
+	newnode->next = *head;
+	newnode->prev = NULL;
+	*head = newnode;
+}
